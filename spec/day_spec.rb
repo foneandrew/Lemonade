c22da01058834e27e23b5473d0601213cb4c8273
Andy Fone <andyfone@Andys-MacBook-Pro-2.local>
2014-11-19 17:03:38 +1300

    added initial files for the Day class and its tests

---
+++ spec/day_spec.rb
@@ -0,0 +1,8 @@
+require_relative '../model/day'
+
+RSpec.describe Day do
+  #Day should be created
+  #be immutable
+  #have the temperature and number of people who walk past ready for the user to look at
+  #ANOTHER CLASS WILL RECIEVE THE WALKERS AND LEMONADE PRICE TO DETERMINE BUYERS
+end
